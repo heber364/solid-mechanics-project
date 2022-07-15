82a49df7b47fde27e2652bf7dfbb1f1960622438
Héber Lima Silva <heberlimasilva@gmail.com>
2022-07-15 11:12:31 -0300

    Corrigindo deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,6 @@ import { Tabs } from "../components/tabs"
 
 import { useEffect, useState } from "react";
 
-import { Chart } from "react-google-charts";
 import _ from "lodash";
 import produce from "immer";
 
